abf5bbc4a64ea162bf3c691298e74a8fabd4ecd9
mohamed-y <24-mohamedyehia>
2025-10-31 21:04:09 +0200

    implement audio transcription and summarization features in WelcomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'welcome_screen.dart'; 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    print('Error loading .env file: $e');
+  }
   runApp(const ScribeAIApp());
 }
+
 class ScribeAIApp extends StatelessWidget {
   const ScribeAIApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'ScriptAI',
       theme: ThemeData(
